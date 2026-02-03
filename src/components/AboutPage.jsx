41c12c93a4e625b6329795c4ca6b9da6282ecc98
Jibran Syed <jibran.syed77@gmail.com>
2026-02-02 22:50:16 -0500

    update

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,21 @@
+import Header from "./Header";
+
+const AboutPage = () => (
+  <>
+    <Header />
+    <h2>About Us</h2>
+    <p>
+      Welcome to <strong>Paradise Nursery</strong>!<br /><br />
+      Founded in 2024, Paradise Nursery is your local haven for all things green and growing. 
+      We are passionate plant lovers dedicated to helping you create your own indoor paradise, 
+      whether you’re a seasoned gardener or just starting your plant journey.<br /><br />
+      Our curated selection features tropical beauties, hardy succulents, and vibrant flowering plants, 
+      all hand-picked for quality and health. Our knowledgeable staff is always ready to offer advice, 
+      care tips, and inspiration.<br /><br />
+      At Paradise Nursery, we believe every home deserves a touch of nature. 
+      Thank you for letting us be part of your plant adventure!
+    </p>
+  </>
+);
+
+export default AboutPage;
